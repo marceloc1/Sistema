f4c6eb9a023c7c765c932b7f720541ea796ac4a6
marceloc1 <marcelo.oo@hotmail.com>
2024-01-27 10:49:47 -0300

    reformulando form do cadastro de clientes

--- Projeto delphi/ufrmCadastroCliente.pas
+++ Projeto delphi/ufrmCadastroCliente.pas
@@ -4,16 +4,26 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.DBCtrls;
 
 type
   TfrmCadastroCliente = class(TForm)
     Panel1: TPanel;
-    btnConfirmarCadastroCliente: TButton;
+    btnGravarCadastroCliente: TButton;
     btnCancelar: TButton;
-    Label1: TLabel;
-    Label2: TLabel;
-    Label3: TLabel;
+    lblNome: TLabel;
+    lnlEndereco: TLabel;
+    lblTipoPessoa: TLabel;
+    lblNumero: TLabel;
+    lblBairro: TLabel;
+    lblCPF: TLabel;
+    lblRG: TLabel;
+    lblEmail: TLabel;
+    lblTelefone: TLabel;
+    lblCelular: TLabel;
+    chkParceria: TDBCheckBox;
+    lblSobrenome: TLabel;
+    lblEmail2: TLabel;
   private
     { Private declarations }
   public
